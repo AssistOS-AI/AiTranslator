652a6342b821a70c8cd4d0da1fa1c7fd56de1fff
Koroem <conduratvictor@yahoo.com>
2024-01-09 11:13:28 +0200

    updates

--- manager.js
+++ manager.js
@@ -1,12 +1,12 @@
 import {routingService} from "./services/routingService.js";
-
 export class Manager {
     constructor() {
         this.appName = "AiTranslator";
-        this.services = new Set();
-        this.services.routingService = new routingService();
+        this.services = new Map();
+        this.services.set('routingService', new routingService());
     }
+
     async navigateToLocation(location) {
-        this.services.routingService.navigateToLocation(location, this.appName);
+        this.services.get('routingService').navigateToLocation(location, this.appName);
     }
 }
